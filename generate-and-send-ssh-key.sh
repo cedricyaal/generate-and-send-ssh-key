8e6c13182bcb88d383dec617390d9e959391917a
cedricyaal <cedric@yaal.fr>
2018-05-30 11:24:34 +0200

    Adding a new flag -hf

--- generate-and-send-ssh-key.sh
+++ generate-and-send-ssh-key.sh
@@ -7,6 +7,7 @@ FILENAME=~/.ssh/id_test
 KEYTYPE=rsa
 HOST=host
 USER=${USER}
+HOSTS_FILENAME=test
 
 # use "-p <port>" if the ssh-server is listening on a different port
 SSH_OPTS="-o PubkeyAuthentication=no"
@@ -20,6 +21,7 @@ function usage() {
     echo "  -u(--user) <username>, default: ${USER}"
     echo "  -f(--file) <file>, default: ${FILENAME}"
     echo "  -h(--host) <hostname>, default: ${HOST}"
+    echo "  -hf(--hosts_file) <hostnames_file>, default: ${HOSTS_FILENAME}"
     echo "  -p(--port) <port>, default: <default ssh port>"
     echo "  -k(--keysize) <size>, default: ${KEYSIZE}"
     echo "  -t(--keytype) <type>, default: ${KEYTYPE}"
@@ -37,31 +39,35 @@ do
 	key="$1"
 	shift
 	case $key in
-		-u*|--user)
+		-u|--user)
 			USER="$1"
 			shift
 			;;
-		-f*|--file)
+		-f|--file)
 			FILENAME="$1"
 			shift
 			;;
-		-h*|--host)
+		-h|--host)
 			HOST="$1"
 			shift
 			;;
-		-p*|--port)
+		-hf|--hosts_file)
+			HOSTS_FILENAME="$1"
+			shift
+			;;
+		-p|--port)
 			SSH_OPTS="${SSH_OPTS} -p $1"
 			shift
 			;;
-		-k*|--keysize)
+		-k|--keysize)
 			KEYSIZE="$1"
 			shift
 			;;
-		-t*|--keytype)
+		-t|--keytype)
 			KEYTYPE="$1"
 			shift
 			;;
-		-P*|--passphrase)
+		-P|--passphrase)
 			PASSPHRASE="$1"
 			shift
 			;;
@@ -71,10 +77,6 @@ do
 			;;
 	esac
 done
-
-echo "Transferring key from ${FILENAME} to ${USER}@${HOST} using options '${SSH_OPTS}', keysize ${KEYSIZE} and keytype: ${KEYTYPE}"
-echo "Press enter to continue or CTRL-C to abort"
-read
 
 # check that we have all necessary parts
 SSH_KEYGEN=`which ssh-keygen`
@@ -90,54 +92,78 @@ if [ -z "$SSH" ];then
     exit 1
 fi
 
-# perform the actual work
-if [ -f $FILENAME ]
-then
-    echo Using existing key
-else
-    echo Creating a new key using $SSH-KEYGEN
-    $SSH_KEYGEN -t $KEYTYPE -b $KEYSIZE  -f $FILENAME -N "$PASSPHRASE"
+function do_work() {
+    # perform the actual work
+    if [ -f $FILENAME ]
+    then
+        echo Using existing key
+    else
+        echo Creating a new key using $SSH-KEYGEN
+        FILENAME=~/.ssh/${HOST//./-}
+        echo Output Filename $FILENAME
+
+        $SSH_KEYGEN -t $KEYTYPE -b $KEYSIZE  -f $FILENAME -N "$PASSPHRASE"
+        RET=$?
+        if [ $RET -ne 0 ];then
+            echo ssh-keygen failed: $RET
+            exit 1
+        fi
+    fi
+
+
+    echo "Transferring key from ${FILENAME} to ${USER}@${HOST} using options '${SSH_OPTS}', keysize ${KEYSIZE} and keytype: ${KEYTYPE}"
+    echo "Press enter to continue or CTRL-C to abort"
+    read
+
+    echo Adjust permissions of generated key-files locally
+    chmod 0600 ${FILENAME} ${FILENAME}.pub
     RET=$?
     if [ $RET -ne 0 ];then
-        echo ssh-keygen failed: $RET
+        echo chmod failed: $RET
         exit 1
     fi
-fi
 
-echo Adjust permissions of generated key-files locally
-chmod 0600 ${FILENAME} ${FILENAME}.pub
-RET=$?
-if [ $RET -ne 0 ];then
-    echo chmod failed: $RET
-    exit 1
-fi
+    echo "Creating ssh directory if needed"
+    ssh $SSH_OPTS $USER@$HOST 'mkdir -p ~/.ssh'
 
-echo Copying the key to the remote machine $USER@$HOST, this should ask for the password
-if [ -z "$SSH_COPY_ID" ];then
-    echo Could not find the 'ssh-copy-id' executable, using manual copy instead
-    cat ${FILENAME}.pub | ssh $SSH_OPTS $USER@$HOST 'cat >> ~/.ssh/authorized_keys'
-else
-	$SSH_COPY_ID $SSH_OPTS -i $FILENAME.pub $USER@$HOST
+    echo Copying the key to the remote machine $USER@$HOST, this should ask for the password
+    if [ -z "$SSH_COPY_ID" ];then
+        echo Could not find the 'ssh-copy-id' executable, using manual copy instead
+        cat ${FILENAME}.pub | ssh $SSH_OPTS $USER@$HOST 'cat >> ~/.ssh/authorized_keys'
+    else
+        $SSH_COPY_ID $SSH_OPTS -i $FILENAME.pub $USER@$HOST
+        RET=$?
+        if [ $RET -ne 0 ];then
+          echo Executing ssh-copy-id via $SSH_COPY_ID failed, trying to manually copy the key-file instead
+          cat ${FILENAME}.pub | ssh $SSH_OPTS $USER@$HOST 'cat >> ~/.ssh/authorized_keys'
+        fi
+    fi
+
     RET=$?
     if [ $RET -ne 0 ];then
-      echo Executing ssh-copy-id via $SSH_COPY_ID failed, trying to manually copy the key-file instead
-      cat ${FILENAME}.pub | ssh $SSH_OPTS $USER@$HOST 'cat >> ~/.ssh/authorized_keys'
+        echo ssh-copy-id failed: $RET
+        exit 1
     fi
-fi
 
-RET=$?
-if [ $RET -ne 0 ];then
-    echo ssh-copy-id failed: $RET
-    exit 1
-fi
+    echo Adjusting permissions to avoid errors in ssh-daemon, this will ask once more for the password
+    $SSH $SSH_OPTS $USER@$HOST "chmod go-w ~ && chmod 700 ~/.ssh && chmod 600 ~/.ssh/authorized_keys"
+    RET=$?
+    if [ $RET -ne 0 ];then
+        echo ssh-chmod failed: $RET
+        exit 1
+    fi
 
-echo Adjusting permissions to avoid errors in ssh-daemon, this will ask once more for the password
-$SSH $SSH_OPTS $USER@$HOST "chmod go-w ~ && chmod 700 ~/.ssh && chmod 600 ~/.ssh/authorized_keys"
-RET=$?
-if [ $RET -ne 0 ];then
-    echo ssh-chmod failed: $RET
-    exit 1
-fi
+    # Cut out PubKeyAuth=no here as it should work without it now
+    echo Setup finished, now try to run $SSH `echo $SSH_OPTS | sed -e 's/-o PubkeyAuthentication=no//g'` -i $FILENAME $USER@$HOST
+}
 
-# Cut out PubKeyAuth=no here as it should work without it now
-echo Setup finished, now try to run $SSH `echo $SSH_OPTS | sed -e 's/-o PubkeyAuthentication=no//g'` -i $FILENAME $USER@$HOST
+
+if [ -f $HOSTS_FILENAME ]; then
+    while IFS='' read -r line || [[ -n "$line" ]]; do
+        HOST=$line
+        echo "Setting up: $HOST"
+        do_work
+    done < "$HOSTS_FILENAME"
+else
+    do_work
+fi
